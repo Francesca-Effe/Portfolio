b74ad6f1e9bbc4108ef9b75509c6e48c96c6c9d3
Francecsa-Effe <f.ferrodev@gmail.com>
2024-10-29 20:36:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+ const backpack=document.querySelector('.holder-backpack');
+const shovel=document.querySelector('.holder-shovel');
+const gear=document.querySelector('.holder-gear');
+
+const selImage=document.querySelectorAll('.select-image');
+
+function activateImage(holder) {
+    selImage.forEach(sel => sel.classList.remove('active'));
+    const sel=holder.querySelector('.select-image');
+    if (sel){
+        sel.classList.add('active');
+        
+    } 
+}
+
+function deactivateImage(){
+    selImage.forEach(sel => sel.classList.remove('active'));
+}
+
+
+backpack.addEventListener('mouseover',()=> {
+    activateImage(backpack);
+} );
+shovel.addEventListener('mouseover',()=> {
+    
+     activateImage(shovel)});
+gear.addEventListener('mouseover',()=> {
+    
+     activateImage(gear)});
+
+backpack.addEventListener('mouseout', deactivateImage);
+shovel.addEventListener('mouseout', deactivateImage);
+gear.addEventListener('mouseout', deactivateImage);
+
+window.addEventListener('load',()=>{
+    activateImage(backpack);
+});
+ 
+
+
+
